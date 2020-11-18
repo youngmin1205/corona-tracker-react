710ee3b82b39e34368f8aec472ea9db9ea37ecaa
Young <youngshinmin1205@gmail.com>
2020-11-17 22:10:27 -0500

    add api and material ui

--- src/App.js
+++ src/App.js
@@ -5,13 +5,27 @@ import React from 'react';
 // import CountryPicker from './components/CountryPicker/CountryPicker'
 
 import { Cards, Chart, CountryPicker } from './components';
-import styles from './App.module.css'
+import styles from './App.module.css';
+import { fetchData } from './api';
 
+//all other components are functional with hooks
 class App extends React.Component {
+    //constructor is not neccessary
+    state = {
+        data: {},
+    }
+
+    async componentDidMount() {
+        const fetchedData = await fetchData();
+
+        this.setState({ data: fetchedData }) //populate the data
+    }
+
     render() {
+        const { data } = this.state;
         return (
             <div className = {styles.container}>
-                <Cards />
+                <Cards data = {data}/>
                 <Chart /> 
                 <CountryPicker />
             </div>
